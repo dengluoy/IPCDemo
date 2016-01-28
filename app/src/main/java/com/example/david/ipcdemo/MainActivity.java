abdf235a7c1b5f498ca3b53607569e64bbce53c7
dengluoy <develhello@163.com>
2016-01-29 01:51:39 +0800

    微调

--- app/src/main/java/com/example/david/ipcdemo/MainActivity.java
+++ app/src/main/java/com/example/david/ipcdemo/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.david.ipcdemo;
 
 import android.content.Intent;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private IBinder.DeathRecipient mDeathRecipient = new IBinder.DeathRecipient() {
+        @Override
+        public void binderDied() {
+            if()
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
     public void onClick(View view) {
