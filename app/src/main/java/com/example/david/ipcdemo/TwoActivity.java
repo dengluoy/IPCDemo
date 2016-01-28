abb677e90a858464d50465a7da87c88e46eb8ea2
dengluoy <develhello@163.com>
2016-01-28 22:19:18 +0800

    first commit

---
+++ app/src/main/java/com/example/david/ipcdemo/TwoActivity.java
@@ -0,0 +1,35 @@
+package com.example.david.ipcdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * @author WeiDeng
+ * @FileName com.example.david.ipcdemo.TwoActivity.java
+ * @date 2016-01-28 22:07
+ * @describe
+ */
+public class TwoActivity extends Activity {
+
+    public final String TAG = getClass().getSimpleName();
+
+    private TextView contentTv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text);
+        contentTv = (TextView) findViewById(R.id.content_name_tv);
+        contentTv.setText(TAG);
+
+        contentTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+}
